9bb4a968994b69aa8bd02cec6f688544924edfa6
王科翔 <kx.wang@hotmail.com>
2019-05-15 15:31:11 +0800

    add command argument

--- GraphBuilder/GraphBuilder.cpp
+++ GraphBuilder/GraphBuilder.cpp
@@ -10,27 +10,34 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main() {
+int main(int argc, char **argv) {
     vector<Table *> tables;
     vector<TypeEdge *> potentialEdges;
     ControllerParser controllerParser;
     Dictionary dictionary = *(new Dictionary());
-    string fileName = "../control_pack.txt";
-    controllerParser.readFile(fileName, tables, potentialEdges, dictionary);
-//	controllerParser.checkFile(tables, potentialEdges);
-    TableParser tableParser;
-    Graph graph;
-    for (auto &table :tables) {
-        cout << endl << table->tableName << endl;
-        tableParser.parse(*table, potentialEdges, graph, dictionary);
+    string fileName = argv[1];
+    fstream controlFile = fstream(fileName, fstream::in);
+    if (!controlFile) {
+        std::cout << fileName << "没有被创建!" << std::endl;
+    } else {
+        std::cout << fileName << "已经存在!" << std::endl;
     }
-    string vFileName = "graph.v";
-    string eFileName = "graph.e";
-    graph.outputGraph(vFileName, eFileName, dictionary, true);
+//    controllerParser.readFile(fileName, tables, potentialEdges, dictionary);
+////	controllerParser.checkFile(tables, potentialEdges);
+//    TableParser tableParser;
+//    Graph graph;
+//    for (auto &table :tables) {
+//        cout << endl << table->tableName << endl;
+//        tableParser.parse(*table, potentialEdges, graph, dictionary);
+//    }
+    string vFileName = argv[2] + string("/graph.v");
+    string eFileName = argv[2] + string("/graph.e");
+//    graph.outputGraph(vFileName, eFileName, dictionary, true);
     // TODO check
-    string vDictFileName = "dictV.txt";
-    string eDictFileName = "dictE.txt";
-    string lDictFileName = "dictL.txt";
-    dictionary.printMap(vDictFileName, eDictFileName, lDictFileName);
+    string vDictFileName = argv[3] + string("/dictV.txt");
+    string eDictFileName = argv[3] + string("/dictE.txt");
+    string lDictFileName = argv[3] + string("/dictL.txt");
+    std::cout << vFileName << eFileName << vDictFileName << eDictFileName << lDictFileName << std::endl;
+//    dictionary.printMap(vDictFileName, eDictFileName, lDictFileName);
     return 0;
 }
